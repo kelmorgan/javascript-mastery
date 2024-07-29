bff86282a44fe545a88bcac41e1afc913a57f4d7
kufreudoko <kufre.udoko@stanbicibtc.com>
2024-07-29 17:43:39 +0100

    is prime

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 
 
-function square(number) {
-    return number * number;
+function showPrimes(limit) {
+
+    for (number = 2; number <= limit; number++) {
+
+        if (isPrime(number)) console.log(number);
+
+    }
+}
+
+function isPrime(number) {
+    let isPrime = true;
+    for (let factor = 2; factor < number; factor++) {
+        if (number % factor === 0)
+            isPrime = false;
+        break;
+    }
+    return isPrime;
 }
 
 
-let number = square(2);
-
-
-console.log(number);
+showPrimes(20);
