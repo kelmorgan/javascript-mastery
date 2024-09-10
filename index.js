c4492f4486098bc27d15c67145360d3818eb66b5
kufreudoko <kufre.udoko@stanbicibtc.com>
2024-09-10 13:54:16 +0100

    includes

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 
 
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function includes(array, searchEliment) {
+    for (let number of array)
+        if (number === searchEliment)
+            return true;
 
 
-const array = arrayFromRange(-8, 1);
-
-console.log(array);
-
-function arrayFromRange(min, max) {
-    const array = [];
-
-    while (!array.includes(max))
-        array.push(min++);
-
-    return array;
+    return false;
 }
+
+
+
+console.log(includes(numbers, 20))
