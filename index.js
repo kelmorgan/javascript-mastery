e2e272676b0d9fa059cc97d380deb525ecab5521
kufreudoko <kufre.udoko@stanbicibtc.com>
2024-09-10 12:00:11 +0100

    reduce in arrays

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 
 
 const numbers = [1, 2, 3, 4, 5];
-const numbers2 = [6, 7, 8];
-
-
-//const combine = numbers.concat(numbers2);
-const combine = [...numbers, ...numbers2];
 
 
 
-combine.forEach(number => console.log(number))
+const sum = numbers
+    .reduce((accumulator, currentValue) => accumulator + currentValue);
+console.log(sum)
