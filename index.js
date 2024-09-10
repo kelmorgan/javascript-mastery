9fe78e075293dccdf0f33fc839c718acf915012b
kufreudoko <kufre.udoko@stanbicibtc.com>
2024-09-10 10:22:50 +0100

    finding objects in an array

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 
-
-let priceRangers = [
-    { label: '$', tooltip: 'Inexpensive', minPerPerson: 0, maxPerPerson: 10 },
-    { label: '$$', tooltip: 'Moderate', minPerPerson: 11, maxPerPerson: 20 },
-    { label: '$$$', tooltip: 'Expensive', minPerPerson: 21, maxPerPerson: 50 },
-
+const courses = [
+    { id: 1, name: 'a' },
+    { id: 2, name: 'b' }
 ]
 
-let resturants = [
-    { averagePerPerson: 5 }
-]
+const course = courses.find(course => course.name === 'a')
 
-
-
-
+console.log(course)
