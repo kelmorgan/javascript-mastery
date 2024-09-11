5e4c571d755895a2cf314e3be39fd4c35d658b21
kufreudoko <kufre.udoko@stanbicibtc.com>
2024-09-11 09:24:41 +0100

    functions declaration

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 
 
-const movies = [
-    { title: 'a', year: 2018, rating: 4.5 },
-    { title: 'b', year: 2018, rating: 4.7 },
-    { title: 'c', year: 2018, rating: 3 },
-    { title: 'd', year: 2017, rating: 4.5 }
-];
+let run = function () {
+    console.log('run')
+};
 
+// hoisting is the process of moving funtion declaration to the top this is done automatically
 
-
-const values = movies
-    .filter(movie => movie.year === 2018 && movie.rating >= 4)
-    .sort((a, b) => a.rating - b.rating)
-    .reverse()
-    .map(movie => movie.title);
-
-
-console.log(values)
+run()
