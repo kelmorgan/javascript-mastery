19da08d7da8d23c8088635d06849823518fe6615
kufreudoko <kufre.udoko@stanbicibtc.com>
2024-09-11 10:00:22 +0100

    basic error handling

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ const person = {
         return `${person.firstName} ${person.lastName}`;
     },
     set fullName(value) {
+        if (typeof value !== 'string')
+            throw new Error('Value is not a string')
 
         const parts = value.split(' ');
+
+        if (parts.length !== 2)
+            throw new Error('Enter a first and last name')
+
         this.firstName = parts[0];
         this.lastName = parts[1];
 
@@ -15,7 +21,14 @@ const person = {
 };
 
 
-person.fullName = 'Mosh Smith'
+try {
+
+    person.fullName = ''
+
+} catch (e) {
+    alert(e);
+}
+
 
 
 
