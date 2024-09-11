10cc5d8af7d092d85fe3aab428a9aacd122f2503
kufreudoko <kufre.udoko@stanbicibtc.com>
2024-09-11 09:42:34 +0100

    rest operator

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
+function sum(discount, ...args) {
 
+    console.log(args.reduce((a, b) => a + b) * (1 - discount));
 
-let run = function () {
-    console.log('run')
-};
+}
 
-// hoisting is the process of moving funtion declaration to the top this is done automatically
-
-run()
+sum(0.1, 1, 2, 3, 4, 5, 6, 7, 8, 9)
